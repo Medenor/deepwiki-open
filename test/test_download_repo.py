0e06b95d16a5c8ff650c1f488c2e73e54f7a6253
medenor <60567543+Medenor@users.noreply.github.com>
2025-10-29 19:55:40 +0100

    Restrict Codeberg interactions to public repositories

--- test/test_download_repo.py
+++ test/test_download_repo.py
@@ -1,4 +1,22 @@
+import types
+import sys
+
 import pytest
+
+
+if 'tiktoken' not in sys.modules:
+    dummy_tiktoken = types.ModuleType("tiktoken")
+
+    class _DummyEncoding:
+        def encode(self, text):
+            return [0] * max(len(text), 1)
+
+    def _dummy_encoding(*args, **kwargs):
+        return _DummyEncoding()
+
+    dummy_tiktoken.get_encoding = _dummy_encoding  # type: ignore[attr-defined]
+    dummy_tiktoken.encoding_for_model = _dummy_encoding  # type: ignore[attr-defined]
+    sys.modules['tiktoken'] = dummy_tiktoken
 
 from api import data_pipeline
 
@@ -15,3 +33,10 @@ def test_build_clone_url_rejects_codeberg_tokens():
 
     with pytest.raises(ValueError, match="Codeberg private repositories are not supported"):
         data_pipeline._build_clone_url(repo_url, "codeberg", "secret")
+
+
+def test_get_codeberg_file_content_rejects_tokens():
+    repo_url = "https://codeberg.org/user/repo"
+
+    with pytest.raises(ValueError, match="Codeberg private repositories are not supported"):
+        data_pipeline.get_codeberg_file_content(repo_url, "README.md", access_token="secret")
